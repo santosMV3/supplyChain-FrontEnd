9ac684cabeeaa291fed9995eb86389e49c5b7719
Your Name <you@example.com>
2022-04-28 14:11:08 -0300

    corrigindo a variavel de ambiente para o React

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000',
     timeout: 100000
 });
