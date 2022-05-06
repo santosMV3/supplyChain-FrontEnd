26b7ab68c13e0c2d3fe16fbfa04637d29045d605
Gabriel Augusto dos Santos <gabriel.santos@mv3.com.br>
2022-05-06 10:02:39 -0300

    resolve timeout error

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000',
-    timeout: 999999
+    timeout: 100000
 });
--- src/views/pages/Charts.js
+++ src/views/pages/Charts.js
@@ -1,19 +1,3 @@
-/*!
-
-=========================================================
-* Argon Dashboard PRO React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-pro-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 // javascipt plugin for creating charts
 import Chart from "chart.js";
