5fcec783014e775946efd3cb2b977fb6c4ef4523
Gabriel Augusto dos Santos <gabriel.santos@mv3.com.br>
2022-05-06 10:07:33 -0300

    restoring to timeout

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000',
-    timeout: 100000
+    timeout: 9999999
 });
