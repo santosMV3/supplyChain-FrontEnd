13cc54022c6a98501de929b8079aac966289bd43
santosMV3 <gabriel.santos@mv3.com.br>
2025-01-28 05:16:26 -0300

    Resolverá problemas com função que converte type date para string date american

--- src/utils/conversor.js
+++ src/utils/conversor.js
@@ -39,10 +39,11 @@ export const formatDateTime = (date) => {
 }
 
 export const formatDateAmerican = (date) => {
+    date = new Date(date)
     if (date) {
-        const day = (date.getDate()).toString().padStart(2, "0");
-        const month = (date.getMonth() + 1).toString().padStart(2, "0");
         const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Adiciona zero à esquerda se necessário
+        const day = String(date.getDate()).padStart(2, '0');
         return `${year}-${month}-${day}`;
     }
     return false;
